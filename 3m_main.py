6cf8e3fd8d4ee29047ad0cecb04319644efff79f
oopsodie <46882250+oopsodie@users.noreply.github.com>
2019-07-29 04:46:18 +0800

    Add files via upload

--- 3m_main.py
+++ 3m_main.py
@@ -1,55 +1,50 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 import time
-#import urllib3
 from time import gmtime, strftime
-#from urllib.request import urlopen
-#from serial import Serial
 import os
 import ctypes as ct
 import serial
 import schedule
-#from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+import sys
+import struct
+import binascii
+sys.path.append("/home/pi/3meters")
+from config_3m import *
 
+def float_to_hex(f):
+	return hex(struct.unpack('<I',struct.pack('<f',f))[0])
 
 def Hash(data):
-#    data = ct.c_uint16(data)
-    m = int("0xa1b2c3d4",0)
-    seed = int("0x1a2b3c4d",0)
+    #m = int("0xa1b2c3d4",0) #this is m for testing server
+    #seed = int("0x1a2b3c4d",0) #this is seed for testing server
+    m = int("0x0a9d9c40",0)
+    seed = int("0xd5956517",0)
     r = 24
     data=str(data)
     length = len(str(data))
     h = seed ^ length
-#    h = ct.c_uint16(h)
     currentIndex = 0
     dd = bytes(data, encoding = "utf8")
     ind = 0
     while length >= 4:
         k = dd[ind]
-#        print("1--k="+str(k))
         ind += 1
         k|= (dd[ind] << 8)
-#        print("2--k="+str(k))
         ind += 1
         k|= (dd[ind] << 16)
-#        print("3--k="+str(k)) 
         ind += 1
         k|= (dd[ind] << 24)
-#        print("4--k="+str(k))  
-#        print("m="+str(m)+",length="+str(length)+",h="+str(h))  
         k = k * m 
         k = k % 4294967296
-#        print("m="+str(m)+",length="+str(length)+",h="+str(h)+",k="+str(k))  
         k ^= k >> r
-#        print("m="+str(m)+",length="+str(length)+",h="+str(h)+",k="+str(k))  
         k = k * m
         k = k % 4294967296
-#        print("m="+str(m)+",length="+str(length)+",h="+str(h)+",k="+str(k))  
         h *= m
         h = h % 4294967296
-#        print("m="+str(m)+",length="+str(length)+",h="+str(h)+",k="+str(k))  
         h ^= k
         h = h % 4294967296
-#        print("m="+str(m)+",length="+str(length)+",h="+str(h)+",k="+str(k))
         ind += 1  
         length -= 4
     
@@ -73,122 +68,105 @@ def Hash(data):
         h ^= k
         h *= m
         h = h % 4294967296
-#        print("case1: m="+str(m)+",length="+str(length)+",h="+str(h)+",k="+str(k))  
 
     h ^= (h >> 13)
     h *= m
     h = h % 4294967296
     h ^= (h >> 15)
-#    print("m="+str(m)+",length="+str(length)+",h="+str(h)+",k="+str(k))  
 
     return format(h,'X')
 
-
-#print(str(Hash("$ANMR,0912000000,19/06/12-16:14:00,100,00000000,00000000,00000000,436F3720,4386D1EE,437C0C3D,42A26FF6,42A61BCC,429D550A,48339980,00000000")) )   
-
 def at_command(msg_to_server,server_ip,server_port,idn):
-	phone = serial.Serial("/dev/ttyUSB3",119200,timeout=5,rtscts=True,dsrdtr=True)
-	phone.write('AT+CSTT="twm.nbiot"\r')
-	time.sleep(0.5)
-	result = phone.read(1000)
-	print(result)
-	phone.write('AT+CGATT?\r')
-	time.sleep(0.5)
-	result = phone.read(1000)
-	print(result)
-	phone.write('AT+CIICR\r')
-	time.sleep(0.5)
-	result = phone.read(1000)
-	print(result)
-	phone.write('AT+CIFSR\r')
-	time.sleep(0.5)
-	result = phone.read(1000)
-	print(result)
-	phone.write('AT+CIPSTART="TCP","220.133.128.61","48877"\r')
-	#phone.write('AT+CIPSEND\r')
-	#phone.write('$ANMR,0003,18/24/18-09:35:35,099,0000000819,5EAC7F2E\r')
-	time.sleep(0.5)
-	result = phone.read(100)
-	print(result)
-	#phone.write('AT+CIPSEND=0,80\r')
-	#time.sleep(2)
-	#result = phone.read(100)
-	phone.write('AT+CIPSEND=150\r')
-	time.sleep(2)
-	result = phone.read(100)
-	print(result)
-	msg_to_server = str(msg_to_server) + '\n#026\r'
-	phone.write(msg_to_server)
-	#phone.write(0x1a)
-	time.sleep(10)
-	result = phone.read(1000)
-	print(result)
-	#phone.write('AT+CIPPING="8.8.8.8"')
-	#result = phone.read(1000)
-	#print(result)
-	phone.write('AT+CIPCLOSE\r')
-	time.sleep(3)
-	result = phone.read(1000)
-	print(result)
-	phone.write('AT+CIPSHUT\r')
-	time.sleep(3)
-	result = phone.read(1000)
-	print(result)
-	phone.close
+        phone = serial.Serial("/dev/ttyXRUSB1",9600,timeout=5)
+        server_ip = bytes(server_ip, encoding = "utf8")
+        server_port=bytes(server_port, encoding = "utf8")
+        msg_to_server = bytes(str(msg_to_server),encoding="utf8")
+        kk = msg_to_server
+        slen = len(msg_to_server)
+        slen = str(slen)
+        slen = bytes(slen,encoding="utf8")
+        phone.write(b'AT+NRB\r')
+        #print("flag01")
+        time.sleep(20)
+        phone.write(b'AT+NSOCR=STREAM,6,10005,1,AF_INET\r')
+        time.sleep(8)
+        #print("flag03")
+        result = phone.read(1000)
+        print(result)
+        #kk=b'$ANMR,2019039999,19/07/26-02:47:55,100,42de38d5,41b1c6a8,42b1c72b,00000000,00000000,00000000,0000$
+        slen = len(kk)
+        slen = str(slen)
+        slen = bytes(slen,encoding="utf8")
+        #kk=b'$ANMR,2019039999,19/07/26-02:47:55,100,42de38d5,41b1c6a8,42b1c72b,00000000,00000000,00000000,0000$
+        kk=kk.hex()
+        kk=bytes(kk,encoding="utf8")
+        phone.write(b'AT+NSOCO=1,'+server_ip+b','+server_port+b'\r\n')
+        time.sleep(8)
+        #print("flag04")
+        result = phone.read(1000)
+        print(result)
+        phone.write(b'AT+NSOSD=1,'+slen+b','+kk+b'\r\n')
+        time.sleep(8)
+        #print("flag05")
+        result = phone.read(1000)
+        print(result)
+        dresult = result.decode("utf-8")
+        if "UPDATE" in dresult:
+                ll = dresult.split('[')
+                print(ll)
+                nip = ll[1]
+                nip = nip.split(']')
+                nip = nip[0]
+                nport = ll[2]
+                nport = nport.split(']')
+                nport = nport[0]
+                fop = open("/home/pi/3meters/config_3m.py","a")
+                fop.write("ip='"+nip+"'\n")
+                fop.write("port='"+nport+"'\n")
+                fop.close()
+        fop2 = open("/home/pi/3meters/data.log","a")
+        fop2.write(str(msg_to_server)+'\n')
+        fop2.write(dresult)
+        fop2.close()
+        phone.write(b'AT+NSOCL=1\r\n')
+        time.sleep(3)
+        result = phone.read(1000)
+        print(result)
+        phone.close
 
 def get_wq(dev,addr):
 
     client = ModbusClient(method='rtu',port=dev,timeout=1,stopbits=1,bytesize=8,parity='N',baudrate=9600)
     client.connect()
     rr=client.read_input_registers(addr,1,unit=1);
-    #print(rr.registers[0]);
-    return rr
-
-#def read_config_file(cdir):
-
-
-#def write_config_file():
-
-
-
-### set up for sending data
-
-### Default Configuration
-### ip = "220.133.128.61"
-### port = "48877"
-### pid = "2019039999" ### Testing ID
+#    print(rr.registers[0]);
+    return rr.registers[0]
 
 def work3m():
 
-	### Default Configuration
-	ip = "220.133.128.61"
-	port = "48877"
-	pid = "2019039999" ### Testing ID
-
-	### Read Add-on Configuration if exists ###
-	if os.path.isfile("/home/pi/3meters/config_3m.py"):
-		from config_3m import *
-
 	### set up for sending message
 	msg = "$ANMR,"
-	timeQ = strftime("%d/%m/%Y-%H:%M:%S")
+	timeQ = strftime("%y/%m/%d-%H:%M:00")
 	power = '100'
 
 	### Water Meter Data Reading =========
 
-	posQ = '00000000' ### for testing
-	#posQf=get_wq(/dev/WQ,108)
-	#posQ=bytes(posQf, encoding = "utf8")
-
-	negQ = '00000000' ### for testing
-	#negQf=get_wq(/dev/WQ,112)
-	#negQ=bytes(negQf, encoding = "utf8")
-
-	totalQ = '00000000' ### for testing
-	## totalQf=posQf+negQf
-	#totalQ=bytes(totalQf, encoding = "utf8")
-
-	### bytes() need input to be str()?
+	#posQf=get_wq('/dev/WQ',107)+get_wq('/dev/WQ',109)/1000
+	posQf=88.888
+	print(posQf)
+	posQ=float_to_hex(posQf)
+	posQ=posQ.split('0x')
+	posQ=posQ[1]
+	#negQf=get_wq('/dev/WQ',111)+get_wq('/dev/WQ',113)/1000
+	negQf=33.333
+	print(negQf)
+	negQ=float_to_hex(negQf)
+	negQ=negQ.split('0x')
+	negQ=negQ[1]
+	totalQf=posQf-negQf
+	totalQ=float_to_hex(totalQf)
+	totalQ=totalQ.split('0x')
+	totalQ=totalQ[1]
 
 	### ==================================
 
@@ -201,17 +179,16 @@ def work3m():
 	cWalt = '00000000'
 	wLevel = '00000000'
 
-	msg = msg + pid + "," + timeQ + "," + power + "," + timeQ + "," + power + ","
+	msg = msg + pid + "," + timeQ + "," + power + ","
 	msg = msg + posQ + "," + negQ + "," + totalQ + "," + volt1 + "," + volt2 + "," + volt3 + ","
 	msg = msg + cur1 + "," + cur2 + "," + cur3 + "," + cWalt + "," + wLevel
 	crc = Hash(msg)
-	msg = msg + "," + crc
-#	print(msg)
+	msg = msg + "," + crc + "\r\n"
+	print(msg)
 	at_command(msg,ip,port,pid)
 
+work3m()
 
-schedule.every(10).minutes.do(work3m)
-
-while True:
-	schedule.run_pending()
-	time.sleep(1)
+#while True:
+#	schedule.run_pending()
+#	time.sleep(1)
